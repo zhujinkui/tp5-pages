79fcf83413689dd30a819d4c27cdb64f2becd8c1
developer@zhujinkui.com <developer@zhujinkui.com>
2018-09-20 03:02:11 +0800

    update file

--- src/Page.php
+++ src/Page.php
@@ -1,7 +1,7 @@
 <?php
-// 分页类库     
+// 分页类库
 // +----------------------------------------------------------------------
-// | PHP version 5.4+                
+// | PHP version 5.4+
 // +----------------------------------------------------------------------
 // | Copyright (c) 2012-2014 http://www.17php.cn, All rights reserved.
 // +----------------------------------------------------------------------
@@ -10,14 +10,14 @@
 
 namespace think;
 
-class Page 
+class Pages
 {
     public  $page;          //当前页
     public  $total;         //总记录数
     public  $listRows;      //每页显示记录数
     private $uri;           //动态url
     public  $pageNum;       //总页数
-    private $listNum = 6;     //显示页码按钮数量
+    private $listNum = 10;  //显示页码按钮数量
     public  $render;        //分页后的html模板
     public  $data;          //分页后渲染到模板的数据
 
@@ -26,7 +26,7 @@ class Page
      * @param integer  $sdata    [待分页的数据]
      * @param integer $listRows [每页记录数]
      */
-    public function __construct($sdata, $listRows=15)
+    public function __construct($sdata, $listRows = 15)
     {
         $this->total    = count($sdata);
         $this->listRows = $listRows;
@@ -34,13 +34,13 @@ class Page
         $this->page     = !empty($_GET["page"]) ? $_GET["page"] : 1;
         $this->pageNum  = ceil($this->total/$this->listRows);
         $this->render   = $this->pageHtml();
-        $this->data     = array_slice($sdata,($this->page-1)*$this->listRows,$listRows);
+        $this->data     = array_slice($sdata, ($this->page-1)*$this->listRows,$listRows);
         return $this->data;
     }
 
     /**
      * [getUri 动态获取url]
-     * @return string   
+     * @return string
      */
     private function getUri()
     {
@@ -58,53 +58,57 @@ class Page
 
     /**
      * [first 首页]
-     * @return string  
+     * @return string
      */
     private function first()
     {
         $html = "";
-        if($this->page==1)
-            $html.=" <a style='magin=10px;' class='current btn disabled'>首 页</a>";
+        if($this->page == 1)
+            $html .= " <a style='magin=10px;' class='current disabled'>首 页</a>";
         else
-            $html.=" <a class='btn btn-primary-outline' href='{$this->uri}&page=1'>首 页</a>";
+            $html .= " <a class='btn-primary-outline' href='{$this->uri}&page=1'>首 页</a>";
 
         return $html;
     }
 
     /**
      * [prev 上一页]
-     * @return string  
+     * @return string
      */
-    private function prev(){
+    private function prev()
+    {
         $html = "";
-        if($this->page==1)
-            $html.=" <a class='current btn disabled'>上一页</a>";
+        if($this->page == 1)
+            $html.= " <a class='current disabled'>上一页</a>";
         else
-            $html.=" <a class='btn btn-primary-outline' href='{$this->uri}&page=".($this->page-1)."'>上一页</a>";
+            $html.= " <a class='btn-primary-outline' href='{$this->uri}&page=".($this->page-1)."'>上一页</a>";
 
         return $html;
     }
 
     /**
      * [pageList 页码按钮]
-     * @return string  
+     * @return string
      */
-    private function pageList(){
-        $linkPage="";
+    private function pageList()
+    {
+        $linkPage = "";
 
-        $inum=floor($this->listNum/2);
+        $inum     = floor($this->listNum/2);
 
-        for($i=$this->page-$inum;$i<=$this->page+$inum;$i++){
-            if($i<=0){
+        for($i = $this->page-$inum;$i<=$this->page+$inum;$i++){
+            if($i <= 0) {
                 continue;
             }
-            if($i>$this->pageNum){
+
+            if($i>$this->pageNum) {
                 continue;
             }
-            if($i == $this->page){
-                $linkPage.=" <a class='current btn btn-secondary'>{$i}</a>";
-            }else{
-                $linkPage.=" <a class='btn btn-primary-outline' href='{$this->uri}&page={$i}'>{$i}</a>";
+
+            if($i == $this->page) {
+                $linkPage.=" <a class='current btn-secondary' style='color:red;'>{$i}</a>";
+            } else {
+                $linkPage.=" <a class='btn-primary-outline' href='{$this->uri}&page={$i}'>{$i}</a>";
             }
         }
 
@@ -113,48 +117,52 @@ class Page
 
     /**
      * [next 下一页]
-     * @return string  
+     * @return string
      */
-    private function next(){
+    private function next()
+    {
         $html = "";
         if($this->page==$this->pageNum)
-            $html.=" <a class='current btn disabled'>下一页</a>";
+            $html.=" <a class='current disabled'>下一页</a>";
         else
-            $html.=" <a class='btn btn-primary-outline' href='{$this->uri}&page=".($this->page+1)."'>下一页</a>";
+            $html.=" <a class='btn-primary-outline' href='{$this->uri}&page=".($this->page+1)."'>下一页</a>";
 
         return $html;
     }
 
     /**
      * [last 尾页]
-     * @return string  
+     * @return string
      */
-    private function last(){
+    private function last()
+    {
         $html = "";
         if($this->page==$this->pageNum)
-            $html.=" <a class='current btn disabled'>尾 页</a>";
+            $html.=" <a class='current disabled'>尾 页</a>";
         else
-            $html.=" <a class='btn btn-primary-outline' href='{$this->uri}&page=".($this->pageNum)."'>尾 页</a>";
+            $html.=" <a class='btn-primary-outline' href='{$this->uri}&page=".($this->pageNum)."'>尾 页</a>";
 
         return $html;
     }
 
     /**
      * [goPage 输入指定页码]
-     * @return string  
+     * @return string
      */
-    private function goPage(){
-        return '  <input class="input-text" type="text" onkeydown="javascript:if(event.keyCode==13){var page=(this.value>'.$this->pageNum.')?'.$this->pageNum.':this.value;location=\''.$this->uri.'&page=\'+page+\'\'}" value="'.$this->page.'" style="width:52px"><input class="btn btn-secondary" type="button" value="GO" onclick="javascript:var page=(this.previousSibling.value>'.$this->pageNum.')?'.$this->pageNum.':this.previousSibling.value;location=\''.$this->uri.'&page=\'+page+\'\'">  ';
+    private function goPage()
+    {
+        return '  <input class="input-text" type="text" onkeydown="javascript:if(event.keyCode==13){var page=(this.value>'.$this->pageNum.')?'.$this->pageNum.':this.value;location=\''.$this->uri.'&page=\'+page+\'\'}" value="'.$this->page.'" style="width:35px; text-align:center;margin-right:5px;"><input class="btn-secondary" type="button" value="GO" onclick="javascript:var page=(this.previousSibling.value>'.$this->pageNum.')?'.$this->pageNum.':this.previousSibling.value;location=\''.$this->uri.'&page=\'+page+\'\'">  ';
     }
 
     /**
      * [selectPage 选择指定页码]
-     * @return string  
+     * @return string
      */
-    function selectPage(){
-        $inum=10;
-        $location = $this->uri.'&page=';
-        $selectPage ="<span class='va-m'>到第 </span> <span class='select-box' style='width:initial'><select class='select' name='topage' size='1' onchange='window.location=\"$location\"+this.value'>";
+    function selectPage()
+    {
+        $inum       = 10;
+        $location   = $this->uri.'&page=';
+        $selectPage = "<span class='va-m'>到第 </span> <span class='select-box' style='width:initial'><select class='select' style='width:35px !important; height:23px; text-align:center; vertical-align:inherit !important;position: relative;top: 1px;' name='topage' size='1' onchange='window.location=\"$location\"+this.value'>";
 
         for($i=$this->page-$inum;$i<=$this->page+$inum;$i++){
             if($i<=0){
@@ -177,13 +185,14 @@ class Page
 
     /**
      * [pageHtml 组装分页的html模板]
-     * @return string  
+     * @return string
      */
-    function pageHtml(){
-        $html  = "<div class='cl mt-20 text-c'>";
-        // $html .= "<span class='pr-20 va-m'>共有<b>{$this->total}</b>条记录</span>";
-        // $html .= "<span class='pr-20 va-m'>每页显示<b>{$this->listRows}</b>条</span>";
-        // $html .= "<span class='pr-20 va-m'><b>当前{$this->page}/{$this->pageNum}</b>页</span>";
+    function pageHtml()
+    {
+        $html  = "<div class='cl mt-20 text-c' style='margin: 5px 10px 5px 0px;'>&emsp;";
+        $html .= "<span class='pr-20 va-m'>共有 <b>{$this->total}</b> 条记录</span>&emsp;";
+        $html .= "<span class='pr-20 va-m'>每页显示 <b>{$this->listRows}</b> 条</span>&emsp;";
+        $html .= "<span class='pr-20 va-m'><b>当前 {$this->page}/{$this->pageNum} </b>页</span>&emsp;";
 
         $html .= $this->first();
         $html .= $this->prev();
